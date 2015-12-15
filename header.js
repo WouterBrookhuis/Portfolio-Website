eb35bf4134379ba1550033dafad67ddfbe7789a8
colacan <wrdbrookhuis@gmail.com>
2015-12-15 20:19:36 +0100

    First upload

---
+++ header.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*var defaultHeaderFontSize;
+var bodyWidthCutoff = 400;
+var scale;
+
+function calcHeaderTextSize()
+{
+    if($("body").innerWidth() < 400)
+        $("#header h1").css("font-size", scale + "%");
+    else
+        $("#header h1").css("font-size", defaultHeaderFontSize + "px");
+}
+
+$(document).ready(function(){
+    defaultHeaderFontSize =  parseFloat($("#header h1").css("font-size"));
+    scale = 100 * (bodyWidthCutoff / defaultHeaderFontSize);  //100vw = 1px
+    console.log(defaultHeaderFontSize + " \ " + scale);
+    $(window).resize(calcHeaderTextSize);
+    calcHeaderTextSize();
+});*/
